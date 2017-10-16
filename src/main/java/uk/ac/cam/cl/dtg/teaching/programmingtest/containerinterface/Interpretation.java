114ca8039434d550bc08587de1d4e10af0075469
Andrew Rice <acr31@cam.ac.uk>
2017-10-16 11:32:54 +0100

    Whitespace

--- src/main/java/uk/ac/cam/cl/dtg/teaching/programmingtest/containerinterface/Interpretation.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/programmingtest/containerinterface/Interpretation.java
@@ -1,22 +1,18 @@
 /**
- * pottery-container-interface - Within-container API for testing programming
-							exercises
- * Copyright © 2015 Andrew Rice (acr31@cam.ac.uk)
+ * pottery-container-interface - Within-container API for testing programming exercises Copyright ©
+ * 2015 Andrew Rice (acr31@cam.ac.uk)
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>You should have received a copy of the GNU Affero General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package uk.ac.cam.cl.dtg.teaching.programmingtest.containerinterface;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
