240d653ee7f994cb1fc246cbeebe94d005da8173
Andrew Rice <acr31@cam.ac.uk>
2017-10-16 11:32:29 +0100

    Whitespace

--- src/main/java/uk/ac/cam/cl/dtg/teaching/programmingtest/containerinterface/HarnessPart.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/programmingtest/containerinterface/HarnessPart.java
@@ -1,22 +1,18 @@
 /**
- * pottery-container-interface - Within-container API for testing programming
-							exercises
- * Copyright © 2015 Andrew Rice (acr31@cam.ac.uk)
+ * pottery-container-interface - Within-container API for testing programming exercises Copyright ©
+ * 2015 Andrew Rice (acr31@cam.ac.uk)
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>You should have received a copy of the GNU Affero General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package uk.ac.cam.cl.dtg.teaching.programmingtest.containerinterface;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -27,28 +23,28 @@ import java.util.List;
 public class HarnessPart {
 
   /**
-   * A list of strings giving detailed information about what the test did. What objects did it create, what steps did
-   * it follow? Use &lt;code> tags to delimit code for formatting.
-   * For example:
+   * A list of strings giving detailed information about what the test did. What objects did it
+   * create, what steps did it follow? Use &lt;code> tags to delimit code for formatting. For
+   * example:
    *
-   * Created an array &lt;code>a&lt;/code> with 10 elements: &lt;code>1.0&lt;/code>, &lt;code>4.5&lt;/code>,
-   * &lt;code>6.6&lt;/code>, &lt;code>2.1&lt;/code>, &lt;code>6.0&lt;/code>, &lt;code>4.33&lt;/code>,
-   * &lt;code>10.0&lt;/code>, &lt;code>56.3&lt;/code>, &lt;code>100.4&lt;/code>, &lt;code>43.4&lt;/code>.
-   * Called &lt;code>Average.mean(a)&lt;/code>
+   * <p>Created an array &lt;code>a&lt;/code> with 10 elements: &lt;code>1.0&lt;/code>,
+   * &lt;code>4.5&lt;/code>, &lt;code>6.6&lt;/code>, &lt;code>2.1&lt;/code>, &lt;code>6.0&lt;/code>,
+   * &lt;code>4.33&lt;/code>, &lt;code>10.0&lt;/code>, &lt;code>56.3&lt;/code>,
+   * &lt;code>100.4&lt;/code>, &lt;code>43.4&lt;/code>. Called &lt;code>Average.mean(a)&lt;/code>
    */
   private List<String> testSteps;
 
   /**
-   * A brief summary of the test that describes the kind of thing tested rather than the detail of how it was tested.
-   * Use &lt;code> tags to delimit code for formatting.
+   * A brief summary of the test that describes the kind of thing tested rather than the detail of
+   * how it was tested. Use &lt;code> tags to delimit code for formatting.
    *
-   *  For example:
+   * <p>For example:
    *
-   *  Calculated the mean of small arrays containing values in the range of 0 to 1000
+   * <p>Calculated the mean of small arrays containing values in the range of 0 to 1000
    *
-   *  Or:
+   * <p>Or:
    *
-   *  Calculated the mean of small arrays containing a mixture of very small and very large values
+   * <p>Calculated the mean of small arrays containing a mixture of very small and very large values
    */
   private String testSummary;
 
@@ -58,8 +54,12 @@ public class HarnessPart {
   private String errorSummary;
 
   /**
-   * If non-null then an error occurred and this is a detailed breakdown of the error. Use &lt;code> tags to delimit
-   * code, and <ul>, <li> for listing elements (e.g. stack trace)
+   * If non-null then an error occurred and this is a detailed breakdown of the error. Use &lt;code>
+   * tags to delimit code, and
+   *
+   * <ul>
+   *   ,
+   *   <li>for listing elements (e.g. stack trace)
    */
   private String errorDetail;
 
